d5e03b3f0f7f216d4a4f0d3603733beef76168dd
Maciej Moroz <maciej.moroz89@gmail.com>
2019-08-24 00:37:46 +0200

    mob nav menu

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,33 @@
 const BODY = this.document.querySelector("body"),
-  btn_menu = this.document.querySelector("[data-menu-btn]");
+  btn_menu = this.document.querySelector("[data-menu-btn]"),
+  coll = document.getElementsByClassName("colapse");
 
 
 // hamburger menu
 btn_menu.onclick = () => {
   BODY.classList.toggle('open-menu')
 }
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+
+    const content = this.nextElementSibling;
+    if (content.clientHeight = "0px") {
+      content.classList.toggle("active");
+    } else {
+      this.classList.toggle("unactive");
+    }
+  });
+}
+
+
+// for (i = 0; i < coll.length; i++) {
+//   coll[i].addEventListener("click", function () {
+
+//     const content = this.nextElementSibling;
+//     if (content.clientHeight = "0px") {
+//       content.classList.toggle("active");
+//     } else {
+//       this.classList.toggle("unactive");
+//     }
+//   });
+// }
